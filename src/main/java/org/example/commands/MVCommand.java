11159f61e1a35fdd41e920a2e88323d71d3acc44
BelalHendi <belal2872004@gmail.com>
2024-10-29 23:24:52 +0300

    Update MVCommand.java

--- src/main/java/org/example/commands/MVCommand.java
+++ src/main/java/org/example/commands/MVCommand.java
@@ -1,26 +1,16 @@
 package org.example.commands;
 
 import org.apache.commons.io.FileUtils;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.DirectoryStream;
 import static org.apache.commons.io.file.PathUtils.deleteDirectory;
-
+import java.nio.file.*;
 public class MVCommand implements command {
     @Override
     public void execute(String[] args){
         try {
             Path sourcePath = Paths.get(args[0]);
             Path destinationPath = Paths.get(args[0]);
-            if(Files.isDirectory(sourcePath)){
-                FileUtils.copyDirectory(sourcePath.toFile(), destinationPath.toFile());
-                deleteDirectory(sourcePath);
-            }
-            else{
-                Files.copy(sourcePath, destinationPath);
-                Files.delete(sourcePath);
-            }
+            Files.move(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
         }
         catch (Exception e){
             e.printStackTrace();
