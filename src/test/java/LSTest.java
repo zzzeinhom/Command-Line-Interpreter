440da154848dd988e6a724a66ee688f07bde3ff1
Mohammed Zeinhom <mohamedzenhom200@gmail.com>
2024-10-30 17:31:48 +0300

    sry

--- src/test/java/LSTest.java
+++ src/test/java/LSTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LSTest {
-    public void test(String[] args, List<String> expected) {
+    private void test(String[] args, List<String> expected) {
         CDCommand cd = new CDCommand();
         LSCommand ls = new LSCommand();
         cd.execute(new String[]{"LSTest"});
@@ -33,22 +33,22 @@ public class LSTest {
     }
 
     @Test
-    public void testLs() {
+    public void testLS() {
         test(new String[]{""}, List.of("LSTest.txt", "LSTest2.txt", "LSTest3.txt"));
     }
 
     @Test
-    public void testLsAll() {
+    public void testLSAll() {
         test(new String[]{"-a"}, List.of(".LSTest4.txt", ".LSTest5.txt", "LSTest.txt", "LSTest2.txt", "LSTest3.txt"));
     }
 
     @Test
-    public void testLsRecursive() {
+    public void testLSRecursive() {
         test(new String[]{"-r"}, List.of("LSTest.txt", "LSTest2.txt", "LSTest3.txt").reversed());
     }
 
     @Test
-    public void testLsAllRecursive() {
+    public void testLSAllRecursive() {
         test(new String[]{"-a", "-r"}, List.of(".LSTest4.txt", ".LSTest5.txt", "LSTest.txt", "LSTest2.txt", "LSTest3.txt").reversed());
     }
 }
