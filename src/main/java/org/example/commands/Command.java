9420de61f687051bb3c5626dea2cb4537acdbe34
Mohammed Zeinhom <mohamedzenhom200@gmail.com>
2024-10-31 14:53:12 +0300

    fixing Command abs class

--- src/main/java/org/example/commands/Command.java
+++ src/main/java/org/example/commands/Command.java
@@ -3,8 +3,5 @@ package org.example.commands;
 public abstract class Command {
 
     public abstract void execute(String[] args);
-    public abstract String returnResult(String[] args);
-    public abstract void executeWithInput( String[] args, String input );
-
 
 }
